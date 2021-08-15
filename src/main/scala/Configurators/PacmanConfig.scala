393ba1e47243bde6da18ef0a28fe170659928588
TalkingFoxMid <futureinrevolution@gmail.com>
2021-08-15 21:48:18 +0500

    I3 setup

--- src/main/scala/Configurators/PacmanConfig.scala
+++ src/main/scala/Configurators/PacmanConfig.scala
@@ -13,7 +13,7 @@ class PacmanConfigImpl[F[_]: Applicative] extends PacmanConfig[F] {
   private val LinuxBase = Set("base", "linux", "linux-firmware")
   private val ArchFBase = Set("git", "scala", "sbt", "jdk8-openjdk", "pacman-contrib")
   private val Ethernet = Set("dhcpcd")
-  private val MyPackages = Set("vim", "chromium")
+  private val MyPackages = Set("vim", "chromium", "xorg-server", "xorg-xinit", "i3-gaps")
 
   def getBasePackages: F[Set[String]] = (LinuxBase union ArchFBase).pure[F]
 
