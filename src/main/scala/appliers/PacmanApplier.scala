b60a6ef459af68598315d92cdcf631abba79cf18
TalkingFoxMid <futureinrevolution@gmail.com>
2021-08-16 00:01:26 +0500

    Fix: Add package verifying

--- src/main/scala/appliers/PacmanApplier.scala
+++ src/main/scala/appliers/PacmanApplier.scala
@@ -3,19 +3,20 @@ package appliers
 import Funcman.{DiffPackage, PacmanApi, PacmanService}
 import cats.Monad
 import cats.effect.ExitCode
+import cats.effect.kernel.Sync
 import cats.syntax.flatMap._
 import cats.syntax.functor._
 
-class PacmanApplier[F[_]: Monad](implicit pacmanService: PacmanService[F],
-                                 pacmanApi: PacmanApi[F]) extends Applier[F] {
+class PacmanApplier[F[_]: Sync](implicit pacmanService: PacmanService[F],
+                                pacmanApi: PacmanApi[F]) extends Applier[F] {
   override def apply: F[Unit] =
     for {
       _ <- pacmanApi.updateAll
       DiffPackage(toInstall, toDelete) <- pacmanService.getChanges
-      _ <- pacmanApi.removePackage(toDelete)
-      _ <- pacmanApi.installPackage(toInstall)
+//      _ <- pacmanApi.removePackage(toDelete)
+//      _ <- pacmanApi.installPackage(toInstall)
     } yield ()
 }
 object PacmanApplier {
-  def apply[F[_] : Monad](implicit pacmanService: PacmanService[F], pacmanApi: PacmanApi[F]): PacmanApplier[F] = new PacmanApplier
+  def apply[F[_] : Sync](implicit pacmanService: PacmanService[F], pacmanApi: PacmanApi[F]): PacmanApplier[F] = new PacmanApplier
 }
